b10da68da847e09a7869796e54f75be962809e3c
Honza Blaska <honza.blaska@gmail.com>
2025-10-25 19:31:24 +0200

    refactor: gallery detail page

--- src/app/photo-gallery/[id]/page.tsx
+++ src/app/photo-gallery/[id]/page.tsx
@@ -7,17 +7,22 @@ export const revalidate = 60;
 
 type Params = { params: { id: string } };
 
+type GalleryDetailImage = {
+  src: string,
+  desc?: string,
+}
+
 export default async function GalleryDetail({ params }: Params) {
   const supabase = await createClient();
   const id = Number(params.id);
 
-  const { data: item, error } = await supabase
+  const { data: gallery, error } = await supabase
     .from('photo-gallery')
-    .select('id, title, description, created_at, images_urls')
+    .select('id, title, description, created_at, images')
     .eq('id', id)
     .single();
 
-  console.log("Fetched data for detail", item);
+  console.log("Fetched data for detail", gallery);
 
   if (error) {
     console.error(error);
@@ -37,30 +42,33 @@ export default async function GalleryDetail({ params }: Params) {
         &larr; Zpět na Fotogalerii
       </Link>
 
-      <h1 className="mt-8">{item.title}</h1>
-      {item.description && (
-        <p className="text-sm opacity-80 mt-1">{item.description}</p>
+      <h1 className="mt-8">{gallery.title}</h1>
+      {gallery.description && (
+        <p className="text-sm opacity-80 mt-1">{gallery.description}</p>
       )}
       <p className="text-xs opacity-60 mt-1">Vytvořeno:{" "}
-        {item.created_at
-          ? new Date(item.created_at).toLocaleDateString('cs-CZ')
+        {gallery.created_at
+          ? new Date(gallery.created_at).toLocaleDateString('cs-CZ')
           : ''}
       </p>
 
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-6">
-        {Array.isArray(item.images_urls) && item.images_urls.length > 0 ? (
-          item.images_urls.map((url: string, i: number) => (
-            <div key={i} className="relative w-full h-64">
-              <Image
-                src={getImageUrl(url)}
-                alt={`${item.title} #${i + 1}`}
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw,
-                       (max-width: 1200px) 50vw,
-                       33vw"
-                priority={i === 0} // první obrázek priorita
-              />
+        {Array.isArray(gallery.images) && gallery.images.length > 0 ? (
+          gallery.images.map(({src, desc} : GalleryDetailImage, i: number) => (
+            <div key={i}>
+              <div className="relative w-full h-64">
+                <Image
+                  src={getImageUrl(src)}
+                  alt={`${gallery.title} #${i + 1}`}
+                  fill
+                  className="object-cover"
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
+                  priority={i === 0} // první obrázek priorita
+                />
+              </div>
+              {desc && <p>{desc}</p>} 
             </div>
           ))
         ) : (
